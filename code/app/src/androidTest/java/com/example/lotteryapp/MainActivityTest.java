719aa9962283b50d60fe9c2c0bb4154261473a8f
heeya-1star <heeya1@ualberta.ca>
2026-03-12 01:00:45 -0600

    renamed ExampleTest to ~MainActTest and included a test case

---
+++ code/app/src/androidTest/java/com/example/lotteryapp/MainActivityTest.java
@@ -0,0 +1,44 @@
+package com.example.lotteryapp;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario =
+            new ActivityScenarioRule<MainActivity> (MainActivity.class);
+    
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.lotteryapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void navToCreateEventActivity(){
+        onView(withId(R.id.navCreate)).perform(click());
+        onView(withId(R.id.createEventButton)).check(matches(isDisplayed()));
+    }
+}
